42822790978f2a03de1f2e7a2da73e71119b5aa5
ichigomaru00 <apple@sakakiichigo.local>
2014-05-08 13:20:22 +0900

    Initial Commit

---
+++ BallonMapTests/BallonMapTests.m
@@ -0,0 +1,34 @@
+//
+//  BallonMapTests.m
+//  BallonMapTests
+//
+//  Created by bizan.com.mac07 on 2014/05/08.
+//  Copyright (c) 2014年 TeamMusubi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BallonMapTests : XCTestCase
+
+@end
+
+@implementation BallonMapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
