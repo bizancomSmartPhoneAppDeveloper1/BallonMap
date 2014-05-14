bc00b71924a6cc27f4084c8ca3537f2be6288602
ichigomaru00 <apple@sakakiichigo.local>
2014-05-14 20:00:53 +0900

    ログイン画面にチェックボックスを追加

--- BallonMap/LoginViewController.m
+++ BallonMap/LoginViewController.m
@@ -19,10 +19,18 @@
 
 @property (retain, nonatomic) UITextField *username;
 @property (retain, nonatomic) UITextField *password;
+@property (retain, nonatomic) UIButton *checkbox;
 
 @end
 
 @implementation LoginViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    //チェックボックスの状態を読み込む
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    _checkbox.selected = [userDefaults boolForKey:@"checkButton"];
+}
 
 - (void)viewDidLoad
 {
@@ -37,21 +45,44 @@
     loginTableView.backgroundColor = [UIColor clearColor];
     [self.view addSubview:loginTableView];
     
+    //Loginボタン作成
     UIButton *btnLogin = [UIButton buttonWithType:UIButtonTypeSystem];
     btnLogin.backgroundColor = [UIColor whiteColor];
-    btnLogin.frame = CGRectMake(30, 150, 260, 34);
+    btnLogin.frame = CGRectMake(30, 180, 260, 34);
     [btnLogin setTitle:@"Login" forState:UIControlStateNormal];
     [btnLogin addTarget:self action:@selector(btnLoginOnClicked:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btnLogin];
     
+    //サインアップボタン作成
     UIButton *btnSignUp = [UIButton buttonWithType:UIButtonTypeSystem];
     btnSignUp.backgroundColor = [UIColor whiteColor];
-    btnSignUp.frame = CGRectMake(30, 190, 260, 34);
+    btnSignUp.frame = CGRectMake(30, 220, 260, 34);
     [btnSignUp setTitle:@"Sign Up" forState:UIControlStateNormal];
     [btnSignUp addTarget:self action:@selector(btnSignUpOnClicked:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btnSignUp];
+    
+    //チェックボックス作成
+    _checkbox = [[UIButton alloc]initWithFrame:CGRectMake(12,145,14,14)];
+    [_checkbox setImage:[UIImage imageNamed:@"check_off.jpg"] forState:UIControlStateNormal];
+    [_checkbox setImage:[UIImage imageNamed:@"check_on.jpg"] forState:UIControlStateSelected];
+    [_checkbox addTarget:self action:@selector(checkbox:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_checkbox];
+    
+    //ラベル生成
+    UILabel *announceLabel = [[UILabel alloc] initWithFrame:CGRectMake(30,146,150,14)];
+    announceLabel.font = [UIFont boldSystemFontOfSize:11];
+    announceLabel.text = @"ユーザーIDを保存する";
+    [self.view addSubview:announceLabel];
 }
 
+-(void)viewDidAppear:(BOOL)animated{
+    //チェックボックスがONだった場合保存されたusernameを書き込む
+    if (_checkbox.selected) {
+        NSLog(@"username");
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        _username.text = [userDefaults objectForKey:@"username"];
+    }
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -67,12 +98,6 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField
-{
-    [textField resignFirstResponder];
-    return YES;
-}
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 1;
@@ -83,6 +108,7 @@
     return 2;
 }
 
+//username、password用TableViewを作成
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *cellIdentifier = @"cellIdentifier";
@@ -95,8 +121,9 @@
         switch (indexPath.section) {
             case 0:
                 if (indexPath.row == 0) {
-                    self.username = [[UITextField alloc] initWithFrame:CGRectMake(100.0, -1.0, 200.0, 50.0)];
+                    self.username = [[UITextField alloc] initWithFrame:CGRectMake(100, -1, 190, 50)];
                     self.username.returnKeyType = UIReturnKeyDone;
+                    self.username.tag = 0;
                     self.username.placeholder = @"UserID";
                     self.username.delegate = self;
                     self.username.font = [UIFont systemFontOfSize:[UIFont systemFontSize]];
@@ -107,8 +134,9 @@
                         [self.username becomeFirstResponder];
                     }
                 } else if (indexPath.row == 1) {
-                    self.password = [[UITextField alloc] initWithFrame:CGRectMake(100.0, -1.0, 200.0, 50.0)];
+                    self.password = [[UITextField alloc] initWithFrame:CGRectMake(100, -1, 190, 50)];
                     self.password.returnKeyType = UIReturnKeyDone;
+                    self.password.tag = 1;
                     self.password.placeholder = @"Password";
                     self.password.secureTextEntry = YES;
                     self.password.delegate = self;
@@ -122,6 +150,26 @@
         }
     }
     return cell;
+}
+
+#pragma mark -
+#pragma mark textField処理
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    //キーボードを閉じる
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField
+shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    if (textField.tag == 1) {
+        return YES;
+    }
+    //チェックボックスON時にTextfieldに編集があった場合チェックをOFFに
+    if (_checkbox.selected) {
+        _checkbox.selected = NO;
+    }
+    return YES;
 }
 
 #pragma mark -
@@ -176,6 +224,8 @@
     
 }
 
+#pragma mark -
+#pragma mark 画面遷移処理
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     if([[segue identifier] isEqualToString:@"mainView"]) {
         //画面遷移後のビューコントローラを取得
@@ -186,4 +236,21 @@
     }
 }
 
+#pragma mark -
+#pragma mark チェックボックス設定
+-(void)checkbox:(UIButton *)button{
+    //usernameへ入力がされていればチェックボックスをONに出来る
+    if (![_username.text isEqualToString:@""]) {
+        //ボタンの選択状態を設定
+        _checkbox.selected = !_checkbox.selected;
+        //usernameをUserDefaultsへ保存
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        //スイッチの状態を保存
+        [userDefaults setBool:_checkbox.selected forKey:@"checkButton"];
+        //usernameを保存
+        [userDefaults setObject:_username.text forKey:@"username"];
+        [userDefaults synchronize];
+    }
+    NSLog(@"button %d",_checkbox.selected);
+}
 @end
